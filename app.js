2f9647ab112973c9bdbbf0ef941fb643dcc430fb
Chaaran Sen <chaaran.sen@gmail.com>
2017-12-10 22:54:56 +0530

    Added Cors - headers

--- app.js
+++ app.js
@@ -24,6 +24,22 @@ const apiPayload = {
 	},
 	"version": "1.0.0"
 }
+var morgan = require('morgan');            
+var bodyParser = require('body-parser');    
+var cors = require('cors');
+
+app.use(morgan('dev'));                                        
+app.use(bodyParser.urlencoded({'extended':'true'}));            
+app.use(bodyParser.json());                                     
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
 app.get('/apis', (req, res) => res.send(apiPayload))
 app.set("port", process.env.PORT || 3000);
